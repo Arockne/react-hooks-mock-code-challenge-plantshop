7945f1158915dc1fe593c31cb30950e9789c5859
Daniel Calvin <arockne2003@yahoo.com>
2021-09-03 13:57:31 -0700

    Implement a fetch request and render the data to the application

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+
+/*
+As a user:
+
+2. I can add a new plant to the page by submitting the form.
+  //useState within newPlantForm to make a controlled form
+  //on a submit reset the state of the form
+    //also make POST request
+    //reassign plants to have the added plant
+3. I can mark a plant as "sold out".
+4. I can search for plants by their name and see a filtered list of plants.
+*/
+
 function App() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(r => r.json())
+    .then(setPlants)
+  }, [])
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants}/>
     </div>
   );
 }
